a770324bbb6229b982a77c542dc983caeae99c2a
Nathanael Carauna <nathanael.carauna@gmail.com>
2020-09-20 18:05:05 -0300

    Tabela de material criada

---
+++ database/migrations/2020_09_20_193017_create_materiais_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMateriaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materiais', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('codigo', 20);
+            $table->string('nome', 255);
+            $table->text('descricao');
+            $table->integer('quantidade_minima');
+            $table->string('imagem_src', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materiais');
+    }
+}
