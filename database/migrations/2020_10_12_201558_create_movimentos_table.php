ef2033720b3c6b27089350cf757f1595ee2a98a2
Nathanael Carauna <nathanael.carauna@gmail.com>
2020-10-12 17:17:12 -0300

    Criado o migration, o model e o controller de Movimentaçcao

---
+++ database/migrations/2020_10_12_201558_create_movimentos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovimentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movimentos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('operacao');
+            $table->text('descricao');
+
+            // $table->unsignedInteger('user_id')->index();
+            // $table->foreign('user_id')->references('id')->on('users');
+
+            // $table->unsignedInteger('solicitacao_id')->index();
+            // $table->foreign('solicitacao_id')->references('id')->on('solicitacaos');
+
+            $table->unsignedInteger('itemMovimentacao_id')->index();
+            $table->foreign('itemMovimentacao_id')->references('id')->on('itemMovimentacaos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movimentos');
+    }
+}
