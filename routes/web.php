e8956cd9bc57f96cab3340c636707c0325549a24
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-10-19 17:22:25 -0300

    voltando web.php ao estado inicial

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
-
-Route::resource('cargo', 'CargoController');
-
-Route::resource('deposito', 'DepositoController');
-
-Route::resource('usuario', 'UsuarioController');
