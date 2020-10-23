f975dc12098e895140a53981086e7a248ac9fd96
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-10-23 12:57:02 -0300

    add rotas de cargo, deposito, usuario e index (apenas para ambiente de testes)

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/', function () {
     return view('home');
 })->name('home');
 
@@ -28,3 +32,9 @@ Route::post('movimento_saida', 'MovimentoController@saidaStore')->name('moviment
 
 Route::resource('deposito', 'DepositoController');
 Route::get('get_estoques/{deposito_id}', 'DepositoController@getEstoques' )->name('deposito.getEstoque');
+
+Route::resource('cargo', 'CargoController');
+
+Route::resource('deposito', 'DepositoController');
+
+Route::resource('usuario', 'UsuarioController');
