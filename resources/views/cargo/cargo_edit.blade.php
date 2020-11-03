dfc19ba848982530097c5c46bd57b121a9d48156
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-11-02 21:13:09 -0300

    add views de cargo modificadas e integradas com o sistema

--- resources/views/cargo/cargo_edit.blade.php
+++ resources/views/cargo/cargo_edit.blade.php
@@ -8,16 +8,32 @@
 
 @section('content')
 
-    <form action="{{ route('cargo.update', $cargo->id) }}" method="POST">
+    <div style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
+        <h2>EDITAR CARGO</h2>
+    </div>
+
+    <form action="{{ route('cargo.update', $cargo->id) }}" method="POST">   
     
         @csrf
         @method('PUT')
 
-        <label for="nome"> Nome: </label>
-        <input type="text" name="nome" id="nome" value="{{ $cargo->nome }}">
+        <div class="form-group">
+            <label for="nome"> Nome </label>
+            <input class="form-control" type="text" name="nome" id="nome" value="{{ $cargo->nome }}">
+        </div>
 
-        <input type="submit" value="Salvar">
-    
+        <div class="form-row">
+            <div class="col-sm-1">
+                <Button class="btn btn-light" type="button" style="color:#3E3767;" onClick="location.href='../'"> Cancelar </Button>
+            </div>
+            <div class="col-sm-1">
+                <Button class="btn btn-success" type="submit"> Atualizar </Button>
+            </div>
+            <div class="col-sm-1">
+                <Button type="button" class="btn btn-danger"> Remover </Button> <-- Remoção por chamada assicrona -->
+            </div>
+        </div>
+
     </form>
 
 @endsection
--- resources/views/cargo/cargo_index.blade.php
+++ resources/views/cargo/cargo_index.blade.php
@@ -6,30 +6,20 @@
 @endsection
 
 @section('content')
-
-    <a href="{{ route('cargo.create') }} "> <Button> Novo Cargo </Button> </a>
     
-    <table border="1">
-        <thead>
+    <div style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
+        <h2>CARGOS CADASTRADOS</h2>
+    </div>
+
+    <table class="table table-hover">
+        <thead style="background-color: #151631; color: white; border-radius: 15px">
             <th> Nome </th>
-            <th> Mostrar </th>
-            <th> Editar </th>
-            <th> Remover </th>
         </thead>
 
         <tbody>
         @foreach($cargos as $cargo)
-            <tr>
+            <tr onclick="location.href = '{{ route('cargo.edit', ['cargo' => $cargo->id]) }}'" style="cursor: pointer;">
                 <td> {{ $cargo->nome }} </td>
-                <td> <a href="{{ route('cargo.show', $cargo->id) }}"> <Button> Mostrar </Button> </a> </td>
-                <td> <a href="{{ route('cargo.edit', $cargo->id) }}"> <Button> Editar </Button> </a> </td>
-                <td>
-                    <form action="{{ route('cargo.destroy', $cargo->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <Button type="submit"> Remover </Button>
-                    </form>
-                </td>
             </tr>
         @endforeach
         </tbody>
