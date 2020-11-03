cfc270e10debfb91dadd0bbcbd7bc89d1a96bf32
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-11-02 21:20:39 -0300

    add views de usuarios integradas com o sistema (faltam detalhes esteticos)

--- resources/views/usuario/usuario_index.blade.php
+++ resources/views/usuario/usuario_index.blade.php
@@ -7,35 +7,23 @@
 
 @section('content')
 
-    <a href="{{ route('usuario.create') }}"> <Button> Novo Usuário </Button> </a>
-
-    <table border="1">
+    <table>
         <thead>
             <tr>
+                <th> Foto </th>
                 <th> Nome </th>
                 <th> Email </th>
                 <th> Cargo </th>
-                <th> Mostrar </th>
-                <th> Editar </th>
-                <th> Remover </th>
             </tr>
         </thead>
 
         <tbody>
         @foreach($usuarios as $usuario)
             <tr>  
+                <td> <img src="{{ url('storage/img/usuarios/'.$usuario->imagem) }}" alt="{{ $usuario->imagem }}" width="80" height="80"></td>
                 <td> {{ $usuario->nome }} </td>
                 <td> {{ $usuario->email }} </td>
                 <td> {{ $usuario->getCargo($usuario->cargo_id)->nome }} </td>
-                <td> <a href="{{ route('usuario.show', $usuario->id) }}"> <Button> Mostrar </Button> </a> </td>
-                <td> <a href="{{ route('usuario.edit', $usuario->id) }}"> <Button> Editar </Button> </a> </td>
-                <td> 
-                    <form action="{{ route('usuario.destroy', $usuario->id) }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <Button type="submit"> Remover </Button>
-                    </form>
-                </td>
             </tr>
         @endforeach
         </tbody>
