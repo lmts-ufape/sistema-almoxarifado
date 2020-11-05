48b281567148f240253a1b216863488b1949f359
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-11-05 14:11:03 -0300

    add funcionalidades de atualização de registros no bando referentes a tabela de usuarios, juntamente com a sua view usuario_edit, modificada a funcao de atualizacao de nome de img armazenado no bando dentro da classe Img, atualizada as funcionalidades de index pra acessar a view edit de usuarios

--- resources/views/usuario/usuario_edit.blade.php
+++ resources/views/usuario/usuario_edit.blade.php
@@ -1,2 +1,112 @@
 
-@extends('../templates.principal')
+@extends('templates.principal')
+
+@section('title')
+    Editar Usuário
+@endsection
+
+@section('content')
+
+    <div style="border-bottom: #949494 2px solid; padding: 5px; margin-bottom: 10px">
+        <h2>Editar USUÁRIO</h2>
+    </div>
+
+    <form action="{{ route('usuario.update', $usuario->id) }}" enctype="multipart/form-data" method="POST">
+
+        @csrf
+        @method('PUT')
+
+        <div class="form-group">
+
+            <div class="form-group">
+                <h2 class="h4"> Dados Institucionais / Pessoais </h2>
+            </div>
+
+            <figure caption="{{ $usuario->imagem }}">
+            <img src="{{ url('storage/img/usuarios/'.$usuario->imagem) }}" alt="{{ $usuario->imagem }}" width="80" height="80">
+            </figure>
+
+            <div class="form-group">
+                <label for="imagem"> Selecione uma Imagem </label>
+                <input class="form-control-file" type="file" name="imagem" id="imagem" accept=".png, .jpg, .jpeg, .svg, .dib, .bmp">
+            </div>
+            
+            <div class="form-group">
+                <label for="nome"> Nome Completo </label>
+                <input class="form-control" type="text" name="nome" id="nome" placeHolder="Nome Completo" value="{{ $usuario->nome }}">
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-2">
+                    <label for="cpf"> CPF </label>
+                    <input class="form-control" type="text" name="cpf" id="cpf" placeHolder="000.000.000-00" value="{{ $usuario->cpf }}">
+                </div>
+
+                <div class="form-group col-md-2">
+                    <label for="rg"> RG </label>
+                    <input class="form-control" type="text" name="rg" id="rg" placeHolder="00.000.000" value="{{ $usuario->rg }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="data_nascimento"> Data de Nascimento </label>
+                    <input class="form-control" type="date" name="data_nascimento" id="data_nascimento" min="" max="" value="{{ $usuario->data_nascimento }}">
+                </div>
+
+                <div class="form-group col-md-2">
+                    <label for="matricula"> Matrícula do Siga </label>
+                    <input class="form-control" type="number" name="matricula" id="matricula" placeHolder="000000000" value="{{ $usuario->matricula }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="cargo"> Cargo </label>
+                    <select class="custom-select" name="cargo" id="cargo">
+                    <option value="{{ $usuario->cargo_id }}" selected="selected">{{ $usuario->getCargo($usuario->cargo_id)->nome }}</option>
+                    @foreach( $cargos as $cargo )
+                        @if( $cargo->id != $usuario->cargo_id )
+                        <option value="{{ $cargo->id }}"> {{ $cargo->nome }} </option>
+                        @endif
+                    @endforeach
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <h2 class="h4"> Dados de Login </h2>
+            </div>
+
+            <div class="form-group">
+                <label for="email"> Email </label>
+                <input class="form-control" type="email" name="email" id="email" placeHolder="exemplodeemail@upe.br" value="{{ $usuario->email }}">
+            </div>
+
+            <div class="form-group">
+                <label for="senha"> Senha </label>
+                <input class="form-control" type="password" name="senha" id="senha" placeHolder="" value="{{ $usuario->senha }}">
+            </div>
+
+            <div class="form-group">
+                <label for="confimar_senha"> Confirmar Senha </label>
+                <input class="form-control" type="password" name="confirmar_senha" id="confirmar_senha" placeHolder="" value="{{ $usuario->confirmar_senha }}">
+            </div>
+
+            <div class="form-group col-md-12" class="form-row" style="border-bottom: #cfc5c5 1px solid; padding: 0 0 20px 0; margin-bottom: 20px">
+            </div>
+
+            @if($errors->any())
+            <div>
+                <ul>
+                    @foreach($errors->all() as $erro)
+                        <li>{{ $erro }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+
+            <Button class="btn btn-secondary" type="button" onclick="location.href = '../' "> Cancelar </Button>
+            <Button class="btn btn-success" type="submit"> Cadastrar </Button>
+
+        </div>
+
+    </form>
+
+@endsection
--- resources/views/usuario/usuario_index.blade.php
+++ resources/views/usuario/usuario_index.blade.php
@@ -1,5 +1,5 @@
 
-@extends('../templates.principal')
+@extends('templates.principal')
 
 @section('title')
     Usuario
@@ -7,25 +7,31 @@
 
 @section('content')
 
-    <table>
-        <thead>
+    <div style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
+        <h2>USUÁRIOS CADASTRADOS</h2>
+    </div>
+
+    <table class="table table-hover">
+        <thead style="background-color: #151631; color: white; border-radius: 15px">
             <tr>
-                <th> Foto </th>
-                <th> Nome </th>
-                <th> Email </th>
-                <th> Cargo </th>
+                <th scope="col"> Imagem </th>
+                <th scope="col"> Nome </th>
+                <th scope="col"> Email </th>
+                <th scope="col"> Cargo </th>
             </tr>
         </thead>
 
         <tbody>
-        @foreach($usuarios as $usuario)
-            <tr>  
+        @forelse($usuarios as $usuario)
+                <tr onclick="location.href = '{{ route('usuario.edit', ['usuario' => $usuario->id]) }}'" style="cursor: pointer;">
                 <td> <img src="{{ url('storage/img/usuarios/'.$usuario->imagem) }}" alt="{{ $usuario->imagem }}" width="80" height="80"></td>
-                <td> {{ $usuario->nome }} </td>
-                <td> {{ $usuario->email }} </td>
-                <td> {{ $usuario->getCargo($usuario->cargo_id)->nome }} </td>
+                <td>{{ $usuario->nome }}</th>
+                <td>{{ $usuario->email }}</td>
+                <td>{{ $usuario->getCargo($usuario->cargo_id)->nome }}</td>
             </tr>
-        @endforeach
+        @empty
+            <td colspan="5">Sem usuários cadastrados ainda</td>
+        @endempty
         </tbody>
 
     </table>
