abd007d499d15cb0e6eeb1fda806d888086be7d8
Nathanael Carauna <nathanael.carauna@gmail.com>
2020-11-09 17:20:57 -0300

    Seed para cargo de requerente e adm, e seed para login de adm (admin@admin.teste, password)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Cargo;
+use App\Usuario;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+
+        $cargoRequerente = new Cargo();
+        $cargoRequerente->nome = 'requerente';
+        $cargoRequerente->save();
+
+        $cargoAdm = new Cargo();
+        $cargoAdm->nome = 'administrador';
+        $cargoAdm->save();
+
+        $admTeste = new Usuario([
+            'imagem' => 'adm.jpg',
+            'nome' => 'administrador',
+            'cpf' => '00000000000',
+            'rg' => '11111111111',
+            'data_nascimento' => '01/02/2000',
+            'matricula' => '222222222',
+            'cargo_id' => '2',
+            'email' => 'admin@admin.teste',
+            'senha' => '$2y$10$ZGZg6dMzSYegqGT.lKN6deaVsxnMFidwq9Z5f7t3ytaDQ6UE70zFi'
+        ]);
+        $admTeste->save();
+
+
+
+
     }
 }
