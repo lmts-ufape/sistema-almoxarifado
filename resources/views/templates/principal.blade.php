1bc2e96d510ea5cb1d10adb6b1e4de5b71d05ee8
Nathanael Carauna <nathanael.carauna@gmail.com>
2020-11-09 18:03:44 -0300

    Correção dos itens 'inicio' e 'sobre' que não estavam aparecendo quando algum usuário faz login

--- resources/views/templates/principal.blade.php
+++ resources/views/templates/principal.blade.php
@@ -41,14 +41,14 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto" >
                         <!-- Authentication Links -->
-                        @guest
+                        {{-- @guest --}}
                         <li class="nav-item">
                             <a class="nav-link" style="color: white; font-weight: bold" href="{{ route('home') }}">{{ __('Inicio') }}</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" style="color: white; font-weight: bold" href="">{{ __('Sobre') }}</a>
                         </li>
-                        @endguest
+                        {{-- @endguest --}}
                     </ul>
                 </div>
             </div>
