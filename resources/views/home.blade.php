47395cdbfc2ab59161017f1d9bbf2a8098d3652a
Nathanael Carauna <nathanael.carauna@gmail.com>
2020-11-09 00:03:52 -0300

    Layout home sem detalhes brancos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title') Página Inicial @endsection
 
 @section('content')
-<div class="container" style="background-color: #1b1c42; text-align: center; border-radius: 15px; margin: -20px; width: 120%; padding: 200px 20px">
+<div class="container" style="background-color: #1b1c42; text-align: center; border-radius: 10px; margin: -20px; width: 120%; padding: 200px 20px">
     <h1 style="color: #949494; align-items: center">BEM VINDO(A)!</h1>
 </div>
 @endsection
