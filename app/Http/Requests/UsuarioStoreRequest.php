2f26d4c220a09c74ee355f3e637481b643abdd8c
Nathanael Carauna <nathanael.carauna@gmail.com>
2020-11-22 19:52:30 -0300

    validação de cadastro de usuário em português

--- app/Http/Requests/UsuarioStoreRequest.php
+++ app/Http/Requests/UsuarioStoreRequest.php
@@ -36,4 +36,20 @@ class UsuarioStoreRequest extends FormRequest
             'confirmar_senha' => 'required|min:8', // N
         ];
     }
+
+    public function messages()
+    {
+        return[
+            'nome.*' => 'O nome é um campo obrigatório, e deve ter entre 5 e 100 caracteres',
+            'email.*' => 'O email é um campo unico e obrigatório, e deve ter entre 5 e 100 caracteres',
+            'cpf.*' => 'O cpf é um campo obrigatório e deve ter 11 dígitos',
+            'rg.*' => 'O rg é um campo obrigatório e deve ter entre 8 e 11 dígitos',
+            'data_nascimento.*' => 'A data de nascimento é um campo obrigatório',
+            'matricula.*' => 'A matricula é um campo obrigatório e deve ter pelo menos 1 dígito',
+            'cargo.*' => 'Um cargo deve ser selecionado',
+            'imagem.*' => 'O imagem é obrigatória',
+            'senha.*' => 'A senha é um campo obrigatório, e deve ter no mínimo 8 caracteres',
+            'confirmar_senha.*' => 'Confirmação de senha necessária e deve ser igual a senha inserida'
+        ];
+    }
 }
