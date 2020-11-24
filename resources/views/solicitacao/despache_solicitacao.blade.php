75acee0070081828156f305d4eb1d1f81f5225dc
Nathanael Carauna <nathanael.carauna@gmail.com>
2020-11-23 22:33:06 -0300

    termo despachar substituido

--- resources/views/solicitacao/despache_solicitacao.blade.php
+++ resources/views/solicitacao/despache_solicitacao.blade.php
@@ -1,7 +1,7 @@
 
 @extends('templates.principal')
 
-@section('title') Despachar Solicitações @endsection
+@section('title') Atender Solicitações @endsection
 
 @section('content')
     <div style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
