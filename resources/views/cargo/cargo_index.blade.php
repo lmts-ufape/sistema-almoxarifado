07213b6e9a40eda1d60f5d80f3342f538264f465
Nathanael Carauna <nathanael.carauna@gmail.com>
2020-11-23 22:08:46 -0300

    Alteração de cargo para perfil

--- resources/views/cargo/cargo_index.blade.php
+++ resources/views/cargo/cargo_index.blade.php
@@ -6,9 +6,9 @@
 @endsection
 
 @section('content')
-    
+
     <div style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
-        <h2>CARGOS CADASTRADOS</h2>
+        <h2>PERFIS CADASTRADOS</h2>
     </div>
 
     <table id="tableCargos" class="table table-hover table-responsive-md">
