72e02fcff01dd181b4ce9e28d1f73bdaacbd05b4
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-12-01 11:59:00 -0300

    remove upload de imgs da view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -100,20 +100,6 @@
                 </div>
 
                 <div class="form-group row">
-                    <label for="imagem" class="col-md-4 col-form-label text-md-right">{{ __('Imagem') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="imagem" type="file" class="form-control-file @error('imagem') is-invalid @enderror" name="imagem" accept=".png, .jpg, .jpeg, .svg, .dib, .bmp">
-
-                        @error('imagem')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                </div>
-
-                <div class="form-group row">
                     <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
 
                     <div class="col-md-6">
