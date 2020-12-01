577ba85819cb9faa713ddb0a74b8906c3521e0e8
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-12-01 14:12:47 -0300

    add update de dados nome e senha de perfil e senha respectivamente

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -83,13 +83,13 @@ class UsuarioController extends Controller
 
         $validator = Validator::make($request->all(), Usuario::$rules_edit_perfil, Usuario::$messages);
 
-        if($validator->fails()) {
-            redirect()->back()->with('error', ['Error x']);
-        }
-
         // dd($validator);
 
-        // $usuario->fill($data)->Update();
+        $data = [
+            'nome' => $request['nome'],
+        ];
+
+        $usuario->fill($data)->Update();
 
         return redirect()->back()->with('success', 'Perfil atualizado com sucesso!');
     }
@@ -100,7 +100,13 @@ class UsuarioController extends Controller
 
         $validator = Validator::make($request->all(), Usuario::$rules_edit_senha, Usuario::$messages);
 
-        // $usuario->fill($data)->Update();
+        // dd($validator);
+
+        $data = [
+            'senha' => Hash::make($request->senha),
+        ];
+
+        $usuario->fill($data)->Update();
 
         return redirect()->back()->with('success', 'Senha atualizada com sucesso!');
     }
