17dd03ff1117f03ad1de70793bdfc24861f0572e
Alisson Albuquerque <alissonalbuquerque00@gmail.com>
2020-12-04 15:52:41 -0300

    modificando o nome names e ids para password e password_confirmation nos campos de senha e confirmar senha

--- resources/views/usuario/usuario_edit.blade.php
+++ resources/views/usuario/usuario_edit.blade.php
@@ -113,7 +113,7 @@
 
             <div class="form-group">
                 <label for="senha"> Senha contendo ao menos 8 dígitos </label>
-                <input class="form-control @error('senha') is-invalid @enderror" autofocus autocomplete="new-password" type="password" name="senha" id="senha" placeHolder="">
+                <input class="form-control @error('senha') is-invalid @enderror" autofocus autocomplete="new-password" type="password" name="password" id="password" placeHolder="">
                 @error('senha')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
@@ -123,7 +123,7 @@
 
             <div class="form-group">
                 <label for="confimar_senha"> Confirmar Senha </label>
-                <input class="form-control @error('confirmar_senha') is-invalid @enderror" autocomplete="new-password" autofocus type="password" name="confirmar_senha" id="confirmar_senha" placeHolder="">
+                <input class="form-control @error('confirmar_senha') is-invalid @enderror" autocomplete="new-password" autofocus type="password" name="password_confirmation" id="password_confirmation" placeHolder="">
                 @error('confirmar_senha')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
