6efed0afeb6d979c6d942c948feaff66f97cae5f
Edgar_Vinicius <edgar.tecnico@hotmail.com>
2020-12-14 20:28:18 -0300

    Adicionando colunas a tabela de notificacao index

--- resources/views/notificacao/notificacao_index.blade.php
+++ resources/views/notificacao/notificacao_index.blade.php
@@ -6,19 +6,25 @@
     <div style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
         <h2>TODAS AS NOTIFICAÇÕES</h2>
     </div>
-
     <table id="tableNotificacoes" class="table table-hover table-responsive-md">
         <thead style="background-color: #151631; color: white; border-radius: 15px">
         <tr>
             <th class="align-middle" scope="col">Mensagem</th>
+            <th class="align-middle" scope="col">Quantidade Total</th>
+            <th class="align-middle" scope="col">Quantidade Minima</th>
         </tr>
         </thead>
         <tbody>
 
         @forelse($notificacoes as $notificacao)
+            @php
+                $estoque = DB::table('estoques')->where('material_id', '=', $notificacao->material_id)->first();
+            @endphp
             @if(!empty($notificacoes))
             <tr onclick="location.href = '{{route('notificacao.show', ['notificacao_id' => $notificacao->id])}}'" style="cursor: pointer;">
                 <td>{{ $notificacao->mensagem }}</td>
+                <td>{{ $estoque->quantidade }}</td>
+                <td>{{\App\material::all()->find($notificacao->material_id)->quantidade_minima}}</td>
             </tr>
             @endif
         @empty
