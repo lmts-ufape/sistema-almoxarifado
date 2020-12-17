0f984695e171450b3b94a65ba04efe06d93b896d
Nathanael Carauna <nathanael.carauna@gmail.com>
2020-12-17 16:57:49 -0300

    Nome do usuário  logado adicionado à mensagem de boas-vindas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,6 @@
 
 @section('content')
 <div class="container" style="background-color: #1b1c42; text-align: center; border-radius: 10px; margin: -20px; width: 120%; padding: 200px 20px">
-    <h1 style="color: #949494; align-items: center">BEM-VINDO(A)!</h1>
+    <h1 style="color: #949494; align-items: center">BEM-VINDO(A) {{ Str::upper(Auth::user()->nome) }}!</h1>
 </div>
 @endsection
