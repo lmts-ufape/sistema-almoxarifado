a28787a45f52db36bca1c03af266415ffa6e4517
LuisFRL01 <luisfernando.25@hotmail.com>
2020-12-22 12:45:34 -0300

    Atualizar relatorio para abrir em uma nova guia

--- resources/views/relatorio/relatorio_saida_materiais_solicitacoes.blade.php
+++ resources/views/relatorio/relatorio_saida_materiais_solicitacoes.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Relatório</title>
+    <title>Relatório De Saída de Materiais Por Solicitações</title>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
