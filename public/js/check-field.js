8951ebbc4c2d6826f3d1b99985d6398b702e0743
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2021-01-26 17:26:37 -0300

    add restrições de cpf e rg

--- public/js/check-field.js
+++ public/js/check-field.js
@@ -1,7 +1,15 @@
 
-$("#nome").mask("#", {
-    maxlength: false,
-    translation: {
-        '#': {pattern: /[A-zÀ-ÿ ]/, recursive: true}
-    }
+$(document).ready(function(){
+
+    $("#nome").mask("#", {
+        maxlength: false,
+        translation: {
+            '#': {pattern: /[A-zÀ-ÿ ]/, recursive: true}
+        }
+    });
+    
+    $('#cpf').mask('00000000000'); 
+
+    $('#rg').mask('00000000000');
+
 });
