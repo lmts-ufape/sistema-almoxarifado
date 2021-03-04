c8a1d474d4fe34f8702d41708fd30dfc9efecacf
Edgarvital <edgar.tecnico@hotmail.com>
2021-03-04 15:31:29 -0300

    Informações de contato e notificacaofactory

--- resources/views/infos/contato.blade.php
+++ resources/views/infos/contato.blade.php
@@ -1,9 +1,22 @@
 @extends('templates.templateLogin')
 @section('title') Contato @endsection
 @section('content')
-<div class="container" style="background: rgb(240, 238, 238); margin: 30px auto; padding: 40px; border-radius: 15px">
-    <div class="container" style="background: rgb(240, 238, 238); ">
-        <h1 style="color: #3E3767; font-weight: bold; margin-bottom:20px">Contato</h1>
+    <div class="container"
+         style="background: rgb(240, 238, 238); margin: 30px auto; padding: 40px; border-radius: 15px">
+        <div class="container" style="background: rgb(240, 238, 238); ">
+            <h1 style="color: #3E3767; font-weight: bold; margin-bottom:20px">Contato</h1>
+            <h3 style="color: #b91d19; font-weight: bold; margin-bottom:5px">Universidade de Pernambuco (UPE)</h3>
+            <p>
+                R. Cap. Pedro Rodrigues, Bairro São José - CEP: 55294-902 - Garanhuns - PE<br>
+                <b>Email: </b>engenhariadesoftware.multicampi@upe.br</p>
+            <h3 style="color: #1b4b72; font-weight: bold; margin-bottom:5px">Laboratório Multidisciplinar de Tecnologias
+                Sociais (LMTS)</h3>
+            <p>
+                Unidade Acadêmica de Garanhuns - UAG<br>
+                Avenida Bom Pastor, s/n.º, Bairro Boa Vista - CEP: 55292-270 - Garanhuns - PE<br>
+                Laboratório 23<br>
+                <b>Email: </b>lmts@ufrpe.br</p>
+
+        </div>
     </div>
-</div>
 @endsection
